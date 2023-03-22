3082459f43d0b2d437640b55a036f3994c81c005
alemonacoo <ale.monaco@outlook.com>
2023-03-22 17:56:32 +0100

    ordering style files

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import Header from "./components/Header";
-import "./App.css";
+import "./style/App.css";
 import AnimalShow from "./components/AnimalShow";
 
 const getRandomAnimals = () => {
